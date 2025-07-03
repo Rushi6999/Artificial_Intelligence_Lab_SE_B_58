cde352b2b4537989cc088c3f034ccf7fbb1950da
Pritesh Pawar <pritesh.pawar24@mmit.edu.in>
2025-07-03 12:44:29 +0530

    this is calculater

---
+++ Artificial_Intelligence_Lab/test.py
@@ -0,0 +1,23 @@
+# this is calculater 
+def calculate():
+    operation = input("Enter an operator (+, -, *, /): ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error! Division by zero."
+    else:
+        result = "Invalid operation."
+
+    print(f"Result: {result}")
+
+calculate()
